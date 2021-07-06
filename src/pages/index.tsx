c3b4a2fd61b6c242d816e89fa3fb457dd8ab0092
sadnessOjisan <sadness.ojisan@gmail.com>
2021-07-06 19:12:19 +0900

    Fix format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,21 @@ import { VFC } from "react";
 import { repository } from "../repository";
 
 type Props = {
-  data: any
-}
+  data: any;
+};
 
-const TopPage:VFC<Props> = (props) => {
+const TopPage: VFC<Props> = (props) => {
   return <div>hello top page | {JSON.stringify(props)}</div>;
 };
 
-
-
 export const getStaticProps: GetStaticProps<Props> = async (context) => {
-  const data = await repository.getAllItems()
-  console.log(data)
-  return {props: {
-    data
-  }}
-}
+  const data = await repository.getAllItems();
+  console.log(data);
+  return {
+    props: {
+      data,
+    },
+  };
+};
 
-export default TopPage
+export default TopPage;
