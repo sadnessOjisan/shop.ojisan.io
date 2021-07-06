15512c621ef2a4e5af17a1b22f261bfaa4202dc3
Yuta Ide <sadness.ojisan@gmail.com>
2021-07-06 19:14:06 +0900

    Connect Micro CMS (#4)
    * Add new data
    
    * Connect micro cms
    
    * Fix lint
    
    * fix npm
    
    * update package
    
    * Fix format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-const TopPage = () => {
-  return <div>hello top page</div>;
+import { GetStaticProps } from "next";
+import { VFC } from "react";
+
+import { repository } from "../repository";
+
+type Props = {
+  data: any;
+};
+
+const TopPage: VFC<Props> = (props) => {
+  return <div>hello top page | {JSON.stringify(props)}</div>;
+};
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const data = await repository.getAllItems();
+  console.log(data);
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default TopPage;
