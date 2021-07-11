1a45393c919f46e7c25a000ce110d95bb4647253
sadness_ojisan <sadness.ojisan@gmail.com>
2021-07-12 02:24:33 +0900

    update ogp

---
+++ src/const/index.ts
@@ -0,0 +1,6 @@
+export const META_TAG_CONTENT = {
+  siteTitle: "shop.ojisan.io",
+  origin: "https://shop.ojisan.io",
+  description: "sadnessOjisan の不用品をショッピングできるサイトです。",
+  twitterName: "@sadnessOjisan",
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { META_TAG_CONTENT } from "../const";
 
 const css = ` *,
 *:after,
@@ -16,12 +17,33 @@ html {
 body {
   box-sizing: border-box;
 }`;
+
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
           <style>{css}</style>
+          <link rel="icon" type="image/png" href="/favicon.png" sizes="16x16" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta property="og:url" content={`${META_TAG_CONTENT.origin}`} />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={META_TAG_CONTENT.siteTitle} />
+          <meta
+            property="og:description"
+            content={META_TAG_CONTENT.description}
+          />
+          <meta property="og:site_name" content={META_TAG_CONTENT.siteTitle} />
+          <meta
+            property="og:image"
+            content={`${META_TAG_CONTENT.origin}/shop.png`}
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content={META_TAG_CONTENT.twitterName} />
+          <meta name="twitter:creator" content={META_TAG_CONTENT.twitterName} />
         </Head>
         <body>
           <Main />
